0b2d09b57dba97272e32c458c4f9c0f7f66641d7
Antafes <map@wafriv.de>
2019-05-28 11:15:10 +0200

    Add build pipeline

---
+++ ci/check-version.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+VERSION_REPO=`cat ../myxml-pre-release/version`
+VERSION=`cat VERSION`
+
+if [ "$VERSION_REPO" -ne "$VERSION"]
+then
+    echo "New version found."
+    exit 0
+else
+    echo "No new version, nothing to do here."
+    exit 1
+fi
---
+++ ci/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ROOT_FOLDER="$( pwd )/../"
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+echo "Generating symbolic link for cache"
+
+if [ -d "${M2_CACHE}" ] && [ ! -d "${M2_HOME}" ]
+then
+    ln -s "${M2_CACHE}" "${M2_HOME}"
+fi
+
+mvn -Dmaven.test.skip=true deploy
